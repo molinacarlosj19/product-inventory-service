8ee5b2a52010afe77181ffa3e7f8e655fc19d3d4
carlos molina <molinacarlosj@gmail.com>
2025-07-02 12:42:24 -0400

    Line Base

---
+++ src/test/java/com/carlosmolina/productinventoryservice/ProductInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosmolina.productinventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
