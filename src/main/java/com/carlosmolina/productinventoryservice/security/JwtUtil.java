5671254768ecc80c9614dc7d19b52e95779114b5
carlos molina <molinacarlosj@gmail.com>
2025-08-01 10:50:54 -0400

    Fix security.WeakKeyException

---
+++ src/main/java/com/carlosmolina/productinventoryservice/security/JwtUtil.java
@@ -0,0 +1,48 @@
+package com.carlosmolina.productinventoryservice.security;
+
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+    private final long expiration = 86400000; // 1 day
+
+    private SecretKey getSigningKey() {
+        byte[] decodedKey = Base64.getDecoder().decode(secret);
+        return new SecretKeySpec(decodedKey, 0, decodedKey.length, "HmacSHA512");
+    }
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
+                .compact();
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
